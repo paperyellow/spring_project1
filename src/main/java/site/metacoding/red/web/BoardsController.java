7c78f619a995f006862f850cea9a9b2b4a3344b3
paper.yellow <paper.yellow.op@gmail.com>
2022-09-05 11:11:46 +0900

    글쓰기 완료

--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -23,12 +23,15 @@ public class BoardsController {
 
 	@PostMapping("/boards")
 	public String writeBoards(WriteDto writeDto) {
+		// 1번 세션에 접근해서 세션값을 확인한다. 그때 Users로 다운캐스팅한고 키값은 principal로 한다.
 		Users principal = (Users) session.getAttribute("principal");
-
+		// 2번 principal null인지 확인하고 null이면 loginForm 리다이렉션해준다.
 		if (principal == null) {
 			return "redirect:/loginForm";
 		}
-
+		// 3번 BoardsDao에 접근해서 insert 메서드를 호출한다.
+		// 조건 : dto를 entity로 변환해서 인수로 담아준다.
+		// 조건 : entity에는 세션의 principal의 getId가 필요하다.
 		boardsDao.insert(writeDto.toEntitiy(principal.getId()));
 		return "redirect:/";
 	}
@@ -50,7 +53,7 @@ public class BoardsController {
 		if (principal == null) {
 			return "redirect:/loginForm";
 		}
-		
+
 		return "boards/writeForm";
 
 	}
